ed51ec8706f4eb19b174a1df3094eb48a15a3ea9
Voskoboyny <59208948+Voskoboyny@users.noreply.github.com>
2021-02-22 09:39:13 +0300

    kashtan

---
+++ freelans/js/main.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.slider').slick({
+    	nextArrow: '<button type="button" class="slick-btn slick-next">1</button>',
+    	prevArrow: '<button type="button" class="slick-btn slick-prev">2</button>',
+    });
+});
